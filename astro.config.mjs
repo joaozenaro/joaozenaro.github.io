4ae3a2be48d56ee77b5b1c743c84d1904f12a30f
João Zenaro <joaomarcelo661@gmail.com>
2026-06-30 22:24:54 +0000

    npm run format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
-import { unified } from '@astrojs/markdown-remark';
-import { defineConfig } from 'astro/config';
-import remarkToc from 'remark-toc';
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
+import { unified } from '@astrojs/markdown-remark'
+import { defineConfig } from 'astro/config'
+import remarkToc from 'remark-toc'
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,4 +17,4 @@ export default defineConfig({
         }),
     },
     site: 'https://jmz.dev.br',
-});
+})
